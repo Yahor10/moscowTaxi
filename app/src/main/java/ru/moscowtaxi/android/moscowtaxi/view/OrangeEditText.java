d06d2d89e10430183aa57aa6245d126e62eee8fb
Egor Chebotarev <T_EgorC1@verifone.com>
2014-12-15 09:53:50 +0300

    update design

---
+++ app/src/main/java/ru/moscowtaxi/android/moscowtaxi/view/OrangeEditText.java
@@ -0,0 +1,22 @@
+package ru.moscowtaxi.android.moscowtaxi.view;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.util.AttributeSet;
+import android.widget.EditText;
+
+import ru.moscowtaxi.android.moscowtaxi.R;
+
+/**
+ * Created by ychabatarou on 15.12.2014.
+ */
+public class OrangeEditText extends EditText{
+
+    public OrangeEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        int color = context.getResources().getColor(R.color.orange_color);
+        getBackground().setColorFilter(color, PorterDuff.Mode.SRC_ATOP);
+    }
+
+}
